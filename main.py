484a3cf4ea96f954fd2a4c82d37c61907e64cad5
ravindu <ravindutharuka2001@gmail.com>
2024-02-03 00:12:53 +0530

    Added placeholder files

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
 # Router imports
-from Routers.email_filter_router import email_filtering_router
-from Routers.email_summaries_router import email_summaries_router
-from Routers.email_analytics_router import email_analytics_router
-from Routers.email_settings_router import email_settings_router
+from Routers.filter_router import filtering_router
+from Routers.summaries_router import summaries_router
+from Routers.analytics_router import analytics_router
+from Routers.settings_router import settings_router
 #
 
 app = FastAPI()
 
 # Router includes
 email_route_prefix = "/email"
-app.include_router(email_filtering_router, prefix=email_route_prefix)
-app.include_router(email_summaries_router, prefix=email_route_prefix)
-app.include_router(email_analytics_router, prefix=email_route_prefix)
-app.include_router(email_settings_router, prefix=email_route_prefix)
+app.include_router(filtering_router, prefix=email_route_prefix)
+app.include_router(summaries_router, prefix=email_route_prefix)
+app.include_router(analytics_router, prefix=email_route_prefix)
+app.include_router(settings_router, prefix=email_route_prefix)
 #
+
+
+@app.exception_handler(Exception)
+async def general_exception_logger(request, exc):
+
+    raise exc
 
 
 @app.get("/")
