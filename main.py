881de69851170df513fc040bbb6ed7848539461c
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-04-22 01:50:25 +0530

    thread search main parts done - may need improvements

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 import asyncio
 import threading
 import time
+import uvicorn  # debugging
 
 from fastapi import FastAPI
 from api.summary.routes import router as conversation_router
-from api.email_filtering_and_info_generation.routes import router as retrieval_and_info_router
+# from api.email_filtering_and_info_generation.routes import router as retrieval_and_info_router
 from api.settings.routes import router as settings_router
 from api.email_filtering_and_info_generation.read_emails import repeat_every_10mins
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,12 +28,12 @@ app.add_middleware(
 )
 
 app.include_router(conversation_router, prefix="/email")
-app.include_router(retrieval_and_info_router, prefix="/email")
+# app.include_router(retrieval_and_info_router, prefix="/email")
 app.include_router(settings_router,prefix="/email")
 
 
-def run_in_thread():
-    asyncio.run(repeat_every_10mins())
+# def run_in_thread():
+#     asyncio.run(repeat_every_10mins())
     
 # start the continous loop in a new thread
 #threading.Thread(target=run_in_thread, args=(), daemon=True).start()
@@ -41,3 +42,6 @@ def run_in_thread():
 @app.get('/', response_class=RedirectResponse, include_in_schema=False)
 async def docs():
     return RedirectResponse(url='/docs')
+
+if __name__ == "__main__":  # debugging
+    uvicorn.run(app, host="127.0.0.1", port=8000)
