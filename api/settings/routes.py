0ee8f217e5211d01c0cb926b8bb9fc8bc78053c5
Ranindu <raninduharischandra12@gmail.com>
2024-06-26 19:21:36 +0530

    done exception handling in settings page

--- api/settings/routes.py
+++ api/settings/routes.py
@@ -117,52 +117,70 @@ async def receive_criticality_trigger_data(criti_trigger_data: PostingCriticalit
 # notifications channels form listener
 @router.post("/settings/receive_notifications_channel_data")
 async def receive_notification_channel_data(noti_channel_data: PostingNotiSendingChannelsRecord, user=Depends(get_current_user)):
-        username = user.username 
-        dashboardChannelChecked=noti_channel_data.is_dashboard_notifications
-        emailChannelChecked= noti_channel_data.is_email_notifications
+    try:
+        username = user.username
+        dashboardChannelChecked = noti_channel_data.is_dashboard_notifications
+        emailChannelChecked = noti_channel_data.is_email_notifications
+        
         if noti_channel_data.noti_sending_emails is not None and len(noti_channel_data.noti_sending_emails) > 0:
             notiSendingEmails = list(set(noti_channel_data.noti_sending_emails))
         else:
             notiSendingEmails = []
-      
+        
         print(username, dashboardChannelChecked, emailChannelChecked, notiSendingEmails)
         
-        
         if await services.check_user_name_notisending(username):
-          
             await services.update_noti_sending_emails(username, dashboardChannelChecked, emailChannelChecked, notiSendingEmails)
         else:
+            new_noti_sending_email_rec = NotiSendingChannelsRecord(
+                user_name=username,
+                is_dashboard_notifications=dashboardChannelChecked,
+                is_email_notifications=emailChannelChecked,
+                noti_sending_emails=notiSendingEmails
+            )
+            await services.send_notificationchannels_record(new_noti_sending_email_rec)
     
-           new_noti_sending_email_rec = NotiSendingChannelsRecord(user_name=username,is_dashboard_notifications=dashboardChannelChecked, is_email_notifications = emailChannelChecked, noti_sending_emails=notiSendingEmails)
-      
-           await services.send_notificationchannels_record(new_noti_sending_email_rec)
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 400 status code and the error message
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 # system configurations data form listener
 @router.post("/settings/receive_system_configurations_data")
 async def receive_system_configurations_data(system_config_data: SendSystemConfigData):
-        overdue_margin_time = system_config_data.overdue_margin_time
+    try:
+            overdue_margin_time = system_config_data.overdue_margin_time
+            
+            result = collection_configurations.find({"id":1})
+            
+            # if the config doc already exists
+            if result:
+                # combined_products_list = newProducts + result["products"]
+                update_result = collection_configurations.update_one(
+                {"id": 1},
+                {"$set": {"overdue_margin_time": overdue_margin_time}}
+                    )
+            # making a new config doc
+            else:
+                try:
+                        result = collection_configurations.insert_one({
+                            "id": 1,
+                            "overdue_margin_time": overdue_margin_time
+                        })
+                        return {"message": "new config document inserted successfully", "inserted_id": str(result.inserted_id)}
+                except Exception as e:
+                    raise HTTPException(status_code=500, detail=str(e))
+            return {"message": "new config document inserted successfully"}
+    
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
         
-        result = collection_configurations.find({"id":1})
-        
-        # if the config doc already exists
-        if result:
-            # combined_products_list = newProducts + result["products"]
-            update_result = collection_configurations.update_one(
-            {"id": 1},
-            {"$set": {"overdue_margin_time": overdue_margin_time}}
-                )
-        # making a new config doc
-        else:
-            try:
-                    result = collection_configurations.insert_one({
-                        "id": 1,
-                        "overdue_margin_time": overdue_margin_time
-                    })
-                    return {"message": "new config document inserted successfully", "inserted_id": str(result.inserted_id)}
-            except Exception as e:
-                raise HTTPException(status_code=500, detail=str(e))
-        return {"message": "new config document inserted successfully"}
+        # Raise an HTTP exception with a 400 status code and the error message
+        raise HTTPException(status_code=400, detail=str(e))
         
        
         
@@ -189,26 +207,7 @@ async def remove_noti_sending_email(noti_sending_emails_dict: DeleteNotiSendingE
         # If an error occurs, raise an HTTPException with status code 500
         raise HTTPException(status_code=500, detail=str(e))
     
-# # listeing to removal of products       
-# @router.post("/settings/remove_product")
-# async def remove_product(current_reading_products_dict: Dict[str, Any]):
-#     try:
-       
-#         new_products_list=current_reading_products_dict["current_considering_products"]
-        
-#         # Update the noti_sending_emails for the specified user_id
-#         result = collection_configurations.update_one(
-#             {"id": 1},
-#             {"$set": {"products": new_products_list}}
-#         )
-#         if result.modified_count == 1:
-#             return {"message": f"Products updated succesfully."}
-#         else:
-#             raise HTTPException(status_code=500, detail="Failed to update products list")
-   
-#     except Exception as e:
-#         # If an error occurs, raise an HTTPException with status code 500
-#         raise HTTPException(status_code=500, detail=str(e))
+
     
 # listeing to removal of current reading emails        
 @router.post("/settings/remove_reading_email")
@@ -232,93 +231,157 @@ async def remove_reading_email(removing_email_dict: DeleteReadingEmail):
 # send current reading emails to frontend   
 @router.get("/settings/get_current_reading_emails", response_model=List[EmailAccWithNickName])
 async def get_data():
-    reading_email_acc_model_data_array = await get_reading_emails_array()
-    data = [EmailAccWithNickName(address=item["address"], nickname=item["nickname"]) for item in reading_email_acc_model_data_array]
-    return data
+    try:
+        reading_email_acc_model_data_array = await get_reading_emails_array()
+        data = [EmailAccWithNickName(address=item["address"], nickname=item["nickname"]) for item in reading_email_acc_model_data_array]
+        return data
+    except ValueError as ve:
+        print(f"ValueError occurred: {ve}")
+        raise HTTPException(status_code=400, detail="Invalid data format.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="An internal error occurred while retrieving data.")
 
 # send editing email data to frontend   
 @router.get("/settings/get_editing_email_data", response_model=EditingEmailData)
 async def get_editing_email_data(selectedEmail: str = Query(...)):
-    data: EditingEmailData = await services.get_editing_email_data(selectedEmail)
-    return data
+    try:
+        data: EditingEmailData = await services.get_editing_email_data(selectedEmail)
+        return data
+    except ValueError as ve:
+        # Handle specific exceptions like ValueError if needed
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and the error message
+        raise HTTPException(status_code=500, detail="An internal error occurred while retrieving email data.")
 
 
 # send user role data to the frontend
 @router.get("/settings/get_user_role_data", response_model=UserRoleResponse)
 async def get_user_role_data(user=Depends(get_current_user)):
-
-    userroles = user.roles
-    if "Admin" in userroles:
-        data = UserRoleResponse(isAdmin=True)
-    else:
-        data = UserRoleResponse(isAdmin=False)
-        
-    return data
+    try:
+        userroles = user.roles
+        if "Admin" in userroles:
+            data = UserRoleResponse(isAdmin=True)
+        else:
+            data = UserRoleResponse(isAdmin=False)
+            
+        return data
+    except AttributeError as ae:
+        # This exception handles cases where `user` or `user.roles` might not be defined correctly
+        print(f"AttributeError occurred: {ae}")
+        raise HTTPException(status_code=500, detail="Internal server error while retrieving user roles")
+    except HTTPException as he:
+        # Re-raise HTTPExceptions to return the appropriate response to the client
+        raise he
+    except Exception as e:
+        # Catch all other exceptions and log them
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
 
 # send current SS checking emails of user 
 @router.get("/settings/get_current_ss_checking_data", response_model=SSShiftData)
 async def get_current_ss_checking_data(user=Depends(get_current_user)):
+    try:
+        username = user.username 
+        data = await services.get_data_of_ss_threshold(username)
+        return data
+    except Exception as e:
+        # Catch all other exceptions and log them
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
     
-    username = user.username 
-    print("USERNAME--------------------------------------------------------->", username)
-    data = await services.get_data_of_ss_threshold(username)
-   
-    return data
 
 # send current criticality checking emails of user 1
 @router.get("/settings/get_current_criticality_checking_emails", response_model=List[EmailAcc])
 async def get_criticalty_checking_emails(user=Depends(get_current_user)):
-    username = user.username 
-    addresses_string_array = await services.get_accs_to_check_criticality(username)
-    data = [EmailAcc(address=email) for email in addresses_string_array]
-    return data
+    try:
+        username = user.username 
+        addresses_string_array = await services.get_accs_to_check_criticality(username)
+        data = [EmailAcc(address=email) for email in addresses_string_array]
+        return data
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and a generic error message
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while retrieving email accounts.")
 
 # send current criticality checking emails of user 
 @router.get("/settings/get_current_overdue_issues_checking_emails", response_model=List[EmailAcc])
 async def get_current_overdue_issues_checking_emails(user=Depends(get_current_user)):
-    username = user.username 
-    addresses_string_array = await services.get_accs_to_check_overdue_issues(username)
-    data = [EmailAcc(address=email) for email in addresses_string_array]
-    return data
+    try:
+        username = user.username 
+        addresses_string_array = await services.get_accs_to_check_overdue_issues(username)
+        data = [EmailAcc(address=email) for email in addresses_string_array]
+        return data
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and a generic error message
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while retrieving email accounts.")
 
 
 # send current notification channel data of user 
 @router.get("/settings/get_noti_channels_data", response_model=NotiSendingChannelsRecord)
 async def get_noti_channels_data(user=Depends(get_current_user)):
-    username = user.username
-    result = collection_notificationSendingChannels.find_one({"user_name": username})
-    if result:
-        formatted_result =  NotiSendingChannelsRecord(
-        user_name= result['user_name'],
-        is_dashboard_notifications = result['is_dashboard_notifications'],
-        is_email_notifications =result['is_email_notifications'],
-        noti_sending_emails = [EmailAcc(address=email) for email in result['noti_sending_emails']]
-        )
+    try:
+        username = user.username
+        result = collection_notificationSendingChannels.find_one({"user_name": username})
+        if result:
+            formatted_result =  NotiSendingChannelsRecord(
+            user_name= result['user_name'],
+            is_dashboard_notifications = result['is_dashboard_notifications'],
+            is_email_notifications =result['is_email_notifications'],
+            noti_sending_emails = [EmailAcc(address=email) for email in result['noti_sending_emails']]
+            )
+            return formatted_result
+        else:
+            formatted_result = NotiSendingChannelsRecord(
+            user_name = "",
+            is_dashboard_notifications = False,
+            is_email_notifications= False,
+            noti_sending_emails= []
+            )
         return formatted_result
-    else:
-        formatted_result = NotiSendingChannelsRecord(
-        user_name = "",
-        is_dashboard_notifications = False,
-        is_email_notifications= False,
-        noti_sending_emails= []
-         )
-    return formatted_result
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and a generic error message
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while retrieving email accounts.")
     
 # send system configuration data of company
 @router.get("/settings/get_system_configuration_data", response_model=SendSystemConfigData)
 async def get_system_configuration_data():
-    
-    result = collection_configurations.find_one({"id": 1})
-    
-    if result:
-        formatted_result = SendSystemConfigData(overdue_margin_time= result["overdue_margin_time"])
-    else:
-        formatted_result = SendSystemConfigData(overdue_margin_time=14)
-    
-    return formatted_result
+    try:
+        result = collection_configurations.find_one({"id": 1})
+        
+        if result:
+            formatted_result = SendSystemConfigData(overdue_margin_time= result["overdue_margin_time"])
+        else:
+            formatted_result = SendSystemConfigData(overdue_margin_time=14)
+        
+        return formatted_result
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and a generic error message
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while retrieving email accounts.")
         
 @router.get("/settings/get_new_intergrating_email_id", response_model=GetNewIntergratingEmailID)
 async def get_new_intergrating_email_id():
-    
-    return await services.get_new_intergrating_email_id()  
+    try:
+        return await services.get_new_intergrating_email_id()  
+    except Exception as e:
+        # Log the exception (optional)
+        print(f"An error occurred: {e}")
+        
+        # Raise an HTTP exception with a 500 status code and a generic error message
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while retrieving email accounts.")
      
